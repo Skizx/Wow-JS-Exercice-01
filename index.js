27528a05ee1f408b5f4a86403124bcb0d56fa4d6
Kevin <kevin.le.milanais@hotmail.fr>
2022-10-04 11:47:38 +0200

    Ajout de commentaires

--- index.js
+++ index.js
@@ -1,11 +1,15 @@
+//Je crée une variable selectionnant toute les classes panel
 const panels = document.querySelectorAll('.panel');
+//Je crée une boucle pour chaque classe panel j'ajoute un EventListener
 panels.forEach((panel) => {
     panel.addEventListener('click' , () => {
+        //Au clique sur l'élément contenant la classe panel seras alors ajouter/enlever une classe 'active'
         removeActiveClasses();
         panel.classList.add('active');
     });
 });
 
+//Fonction permettant d'enlever la classe 'active'
 function removeActiveClasses() {
     panels.forEach((panel) => {
         panel.classList.remove('active');
